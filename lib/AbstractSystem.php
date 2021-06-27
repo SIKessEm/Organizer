274017b5a49749d2b729e92264ee436929aad6c6
SIGUI kessé Emmanuel <sikessem@gmail.com>
2021-06-27 10:18:22 +0200

    Create the abstract system

---
+++ lib/AbstractSystem.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace SIKessEm\Organizer;
+
+/**
+ * The system abstract class
+ *
+ * @author SIGUI Kessé Emmanuel
+ * @package sikessem/organizer
+ * @license Apache-2.0
+ */
+abstract class AbstractSystem implements SystemInterface {}
